e5004750586fdc0bac02d72552773dc83fbd8a0e
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-24 07:35:47 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let x = 2;
+let y = 'hello'
+
+/*
+number 
+bigint
+string 
+boolean
+null  == standalone value
+undefined 
+symbol == unique 
+object
+*/
+
+console.log(typeof(x))
+console.log(typeof "Amruth")
+console.log(typeof null)
