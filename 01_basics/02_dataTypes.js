2307dc2ffc0627cd8b8f0fdcc5785cd428b22f87
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-25 14:41:37 +0000

    .

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,6 @@
+// primitive (7 types) : string, numeber, boolean, null, undefined, symbol, bigint
+// non-promitive (refernce) : array, objects and functions 
+
 let x = 2;
 let y = 'hello'
 
@@ -15,3 +18,26 @@ object
 console.log(typeof(x))
 console.log(typeof "Amruth")
 console.log(typeof null)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log("id === anotherId :", id === anotherId)
+
+//bigInt
+const largeNumber = 2321334555n
+console.log('type of large number :',typeof(largeNumber))
+
+//Objets
+
+let myObj = {
+    name: 'Amruth',
+    age: 21
+}
+console.log(myObj);
+
+// function
+const myFun = function() {
+    console.log("my Function ")
+}
+console.log(myFun) 
