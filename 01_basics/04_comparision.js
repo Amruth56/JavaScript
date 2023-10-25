bd7329fa4dab4ddbf81c144341cd12e15db37f0c
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-25 12:31:40 +0000

    comaprision for undefined datasets

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -2,3 +2,13 @@ console.log(null >0)
 console.log(null == 0)
 console.log(null >=0)
 console.log(null <=0)
+console.log("2" >1)
+console.log("2" < 1)
+console.log("2" == 2)
+
+console.log("\n")
+ 
+console.log(undefined >0)
+console.log(undefined == 0)
+console.log(undefined >=0)
+console.log(undefined <=0)
