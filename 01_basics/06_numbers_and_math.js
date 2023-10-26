389b2071adc60fc85d2a128331111eb22f919993
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-26 10:04:54 +0000

    strings

---
+++ 01_basics/06_numbers_and_math.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(`score length ${score.length}`)
+console.log(score)
+const typ = typeof(score)
+console.log(`type of ${typ}`)
+
+//new object
+const balance = new Number(100)
+console.log(balance)
+console.log(`type of ${typeof balance}`)
+
+
+console.log(score.toString())
+console.log(typeof score)
+console.log(`score length :${score.toString().length}`) 
