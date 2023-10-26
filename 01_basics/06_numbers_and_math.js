10c6343511287a89472396f6bdeb8fc9c2164b56
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-26 10:29:57 +0000

    numbers

--- 01_basics/06_numbers_and_math.js
+++ 01_basics/06_numbers_and_math.js
@@ -13,3 +13,8 @@ console.log(`type of ${typeof balance}`)
 console.log(score.toString())
 console.log(typeof score)
 console.log(`score length :${score.toString().length}`) 
+
+console.log(`toFixed function it helps to fix the number of decimal numbers: ${score.toFixed(3)}`)
+const otherNum = 12.123456789
+console.log(otherNum.toFixed(2))
+console.log(otherNum.toPrecision(3))
