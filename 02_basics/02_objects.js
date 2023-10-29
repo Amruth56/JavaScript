ef7eabaf8388aeafef083a4d27c5263611210e24
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-29 17:19:06 +0000

    2 ways to access objects in js

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,15 @@
+// singleton
+
+// object literals
+
+const jsuser = {
+    name : "Amruth",
+    "full name": "Amruth Mandappa T S",
+    age: 21,
+    roll: "20bcs013",
+    email: "qwert@gmail.com",
+    timeline: [1,2,3,4]
+}
+
+console.log(jsuser.name)
+console.log(jsuser["full name"])
