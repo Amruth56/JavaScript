1b9ef894b266f6663782459bdbee073ff32223ce
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-30 01:54:53 +0000

    nested objects

---
+++ 02_basics/03_object.js
@@ -0,0 +1,39 @@
+// object singleton 
+
+const whatsappUser1 = new Object();  // singleon objct
+// console.log(whatsappUser1)
+// console.log(`whatsappUser1 :${whatsappUser1}`)
+
+const whatsappUser2 = {} // non singleton objct
+// console.log(whatsappUser2)
+// console.log(`whatsappUser2 :${whatsappUser2}`)
+
+whatsappUser1.id = "123abc"
+whatsappUser1.name = "Sam"
+whatsappUser1.age = 39
+
+// console.log(whatsappUser1)
+
+const instaUser= {
+    name:{
+        userID:{
+            firstname : "Gusto",
+            middlename: "stud",
+            lastname: "muffin"
+        },
+        fullname:{
+            firstname : "Amruth",
+            middlename: "Mandappa",
+            lastname: "T S"
+        }
+
+    },
+    age:21
+}
+console.log("instaUdser :", instaUser)
+console.log("instaUser.name :",instaUser.name)
+console.log("instaUser.name.fullname :",instaUser.name.fullname)
+console.log("instaUser.name.fullname.lastname :",instaUser.name.fullname.lastname)
+console.log("instaUser.name.userID :",instaUser.name.userID, "\n")
+
+// console.log(`nested objects :${instaUser.name}`)
