c4356127dbd07f627015cc183072985a0e483573
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-10-30 10:57:57 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,4 +22,46 @@ function loggedIn(user)
         return `${user}, just logged in `
 }
 // console.log(loggedIn("Amruth"))
-console.log(`${loggedIn()}`)
+console.log(`${loggedIn()}\n`)
+
+
+function calculateCartPrize(...num1) /// (...) is used as a spread operator 
+{
+    return num1;
+}
+console.log(calculateCartPrize(5,4,6,32,1))
+
+function play (val1, val2, ... num1)
+{
+    console.log(`val1 :${val1}, val2 :${val2}, num1 :${num1}`)
+}
+play(1,2,3,4,5,6,7,8,9) 
+
+
+const user = {
+    username : "Amruth",
+    age : 21
+}
+
+function objectWithFunction(anyObject)
+{
+    console.log(`username is ${anyObject.username}, age is ${anyObject.age}, passcode is ${anyObject.passcode}\n`)
+}
+objectWithFunction(user)
+
+function objectWithFunction2(anyObject)
+{
+    console.log(`username is ${anyObject.username}, age is ${anyObject.age}, passcode is ${anyObject.passcode}\n`)
+}
+objectWithFunction2({
+    username: "aske2hf",
+    age: 132,
+    passcode: "34df6"
+})
+
+const newArr = [1,2,4,5,6,7,4,,32,2]
+function retrunSecondValue(getarr)
+{
+    console.log(`value at 2nd index :${getarr[2]}\n`)
+}
+retrunSecondValue(newArr)
