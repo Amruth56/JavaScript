979f4b31d1b50055f4efdab0efbb55fd89d3917d
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-11-07 14:15:44 +0000

    parent function

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,27 @@
+function one()
+{
+    const username = " Amruth"
+
+    function two()
+    {
+        const age = 21
+        console.log(username)
+    }
+    // console.log(age)
+    // two()
+}
+// one()
+
+if(true)
+{
+    const username = " Amruth"
+    if(username === " Amruth")
+    {
+        const age = 21;
+        console.log(username , age)
+    }
+    // console.log(age);
+    console.log(username)
+
+}
+// console.log(username)
