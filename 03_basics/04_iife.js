d9b86747e4f6ff0a6cad82a8ede0a55dae1f8158
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-11-08 02:01:30 +0000

    immediately invoked function expression

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,19 @@
 // Immediately Invoked Function Expression 
+
+//By wrapping the function in parentheses, it creates a new scope, preventing pollution of the global scope.
+(function greetings()
+{
+    console.log("hello everyone")
+})() ;//The function logs “hello everyone” to the console when invoked.
+
+// When you wrap a function in parentheses, it creates a new scope, known as a function scope. This function scope is not accessible from the global scope, which means any variables or functions defined within it won’t be accessible globally.
+
+
+// arrow function 
+(() => {
+    console.log(`DB connected`);
+})();
+
+( (name) => {
+    console.log(`hello ${name}, how are you`)
+})("Amruth")
