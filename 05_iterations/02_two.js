e8ddb91cbc9abd8cfb05077cc00be715e9994a31
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-11-09 14:20:57 +0000

    .

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index < 10) {
+    // console.log(`value is ${index}`)
+    index ++
+}
+
+let arr = ["thor", "Iron Man", "Hulk", "captain America"]
+let cnt =0
+while(cnt < arr.length)
+{
+    // console.log(arr[cnt++]);
+    
+}
+
+//do while loop
+let num =11
+do{
+    console.log(`num :${num}`)
+    num++;
+} while(num < 10)
