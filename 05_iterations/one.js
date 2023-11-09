0dca3094bc7779357342caeab9e5f96f83b1065c
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-11-09 13:31:42 +0000

    .

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -2,8 +2,13 @@ for (let index = 0; index <= 10; index++) {
 
     if(index == 5)
     {
-        console.log("the number is 5")
+        // console.log("the number is 5")
     }
-    console.log(index)
-    
+    // console.log(index)   
+}
+
+const myarr = ["flash", "batman", "superman", "wonder women"];
+for(let i=0; i<myarr.length; i++)
+{
+    console.log(myarr[i])
 }
