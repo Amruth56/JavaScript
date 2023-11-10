2b14221d337b0c8f43b8567f0dec0f163621d244
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-11-10 10:55:04 +0000

    .

--- 05_iterations/05_five.js
+++ 05_iterations/05_five.js
@@ -1,1 +1,44 @@
 //for each loop
+
+const arr = [1, 2, 3, 4, "a", "b", "c", "abc"]
+
+arr.forEach (function (x) {
+    // console.log(x)
+})
+
+arr.forEach((y) => {
+    // console.log(y)
+})
+
+function printme(d)
+{
+    // console.log(d)
+}
+
+arr.forEach(printme)
+
+arr.forEach((item, index, arr) => {
+    // console.log(item, index, arr)
+})
+
+const prog = [ 
+    {
+        language : "JavaScript",
+        fileName : "js"
+    },
+    {
+        language : "C plus plus",
+        fileName : "cpp"
+    },
+    {
+        language : "python",
+        fileName : "py"
+    }
+]
+
+prog.forEach( (x) => {
+    console.log((x.fileName))
+})
+prog.forEach( (x) => {
+    console.log((x))
+})
