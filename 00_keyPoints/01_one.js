a428c480a51115155cb94eb524d8b884f11677cb
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2023-11-15 02:49:50 +0000

    events

---
+++ 00_keyPoints/01_one.js
@@ -0,0 +1,9 @@
+// JavaScript is synchronous -> Each code is executes one after another 
+// JS is single thread 
+/*
+
+blocking code : block the flow of program execution
+non blocking code : does not block the flow of program exection 
+
+
+*/
